dd60fb28b3596d1fcd3333f48e2b9028a924bcc5
juan2ramos <juan2ramos@gmail.com>
2015-03-03 20:42:46 -0500

    Archivos de configuracion

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="es">
+<head>
+
+    <title>Clofa</title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="author" content="juan2ramos"/>
+    <meta name="description" content="Clofa"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/normalize.min.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+
+    <script src="js/prefixfree.min.js"></script>
+
+    <link
+        href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+
+</body>
+<!-- JavaScript -->
+
+<script src="js/jquery-1.11.2.min.js"></script>
+<script src="js/main.js"></script>
+
+</html>
